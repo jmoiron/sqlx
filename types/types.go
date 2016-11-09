041545bf62abecf453690b277aab9edf05d54e38
Thinh Nguyen <tnguyenx91@gmail.com>
2016-11-09 13:55:52 -0500

    Add NullJSONText type

--- types/types.go
+++ types/types.go
@@ -121,6 +121,32 @@ func (j JSONText) String() string {
 	return string(j)
 }
 
+// NullJSONText represents a JSONText that may be null.
+// NullJSONText implements the scanner interface so
+// it can be used as a scan destination, similar to NullString.
+type NullJSONText struct {
+	JSONText
+	Valid bool // Valid is true if JSONText is not NULL
+}
+
+// Scan implements the Scanner interface.
+func (n *NullJSONText) Scan(value interface{}) error {
+	if value == nil {
+		n.JSONText, n.Valid = _EMPTY_JSON, false
+		return nil
+	}
+	n.Valid = true
+	return n.JSONText.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (n *NullJSONText) Value() (driver.Value, error) {
+	if !n.Valid {
+		return nil, nil
+	}
+	return n.JSONText.Value()
+}
+
 // BitBool is an implementation of a bool for the MySQL type BIT(1).
 // This type allows you to avoid wasting an entire byte for MySQL's boolean type TINYINT.
 type BitBool bool

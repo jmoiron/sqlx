c8371f2276cbea35bcd08e49c10e74fb4c2aaaeb
祝黄清 <huangqing.zhu@centurygame.com>
2021-04-09 19:49:29 +0800

    fix: form bulk upsert query

--- named.go
+++ named.go
@@ -224,7 +224,7 @@ func bindStruct(bindType int, query string, arg interface{}, m *reflectx.Mapper)
 	return bound, arglist, nil
 }
 
-var valueBracketReg = regexp.MustCompile(`(?i)VALUES\s*(\([^(]*.[^(]\))`)
+var valueBracketReg = regexp.MustCompile(`(?i)\)\s*VALUES\s*(\([^(]*.[^(]\))`)
 
 func fixBound(bound string, loop int) string {
 
